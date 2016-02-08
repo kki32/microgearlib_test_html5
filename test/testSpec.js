829eb582f3800e36839e84e9e02146d34ac0e166
kki32 <p107.kitisopakul@gmail.com>
2016-02-08 09:42:51 +0700

    to run on jenkins

---
+++ test/testSpec.js
@@ -0,0 +1,13 @@
+define(['Microgear'], function(microgear) {
+describe('Array', function(){
+    it('should return -1 when the value is not present', function(){
+           var microgear = Microgear.create({
+               key: "appkey",
+               secret: "appsecret"
+           });
+           console.log(microgear);
+  
+
+    })
+})
+});

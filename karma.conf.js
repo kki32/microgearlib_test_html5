0713560b2d94423e397e044b619a5d23ee7571cb
kki32 <p107.kitisopakul@gmail.com>
2016-02-08 09:51:56 +0700

    make karma run from local

--- karma.conf.js
+++ karma.conf.js
@@ -37,7 +37,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress','junit', 'dots'],
+    reporters: ['progress'],
 
 
     // web server port
@@ -65,9 +65,7 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: true,
-junitReporter = {
-  outputFile: 'test-results.xml'
-},
+
 
     // Concurrency level
     // how many browser should be started simultaneous
